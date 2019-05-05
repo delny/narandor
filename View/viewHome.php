b0fbbf7e7ae7ab743866b028ddf1d8050e1f934f
anthony <delgado.anthony@gmail.com>
2019-05-05 11:03:31 +0200

    update date

--- View/viewHome.php
+++ View/viewHome.php
@@ -7,7 +7,7 @@ $titre = WEBSITE_TITLE.' -- Accueil';
   <div id="corps" >
     <div class="welcome">
       <h1>Narandor<br /> (version Beta 0.8)</h1>
-      <h2>Derni&egrave;re mise &agrave; jour : 26 novembre 2016</h2>
+      <h2>Derni&egrave;re mise &agrave; jour : 5 mai 2019</h2>
       <div><p>Se connecter / Cr&eacute;er un compte</p></div>
       <?php if (!empty($erreur)): ?>
         <div class="error"><?php print $erreur ?></div>
